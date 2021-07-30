d1f5b0603bb95b5cdbf017c84cc7878edf387280
patwo13 <fathulariff@gmail.com>
2021-07-31 02:12:42 +0800

    Authentication Level 4

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
 const app = express();
 
@@ -42,36 +44,41 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register", function(req,res){
-  const user = new User({
-    email: req.body.username,
-    //password: req.body.password
-    password: md5(req.body.password)
-  });
-
-  user.save(function(err){
-    if(!err){
-      console.log("Successfully registerd");
-      res.render("secrets");
-    }
-    else{
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password, saltRound, function(err, hash){
+    const user = new User({
+      email: req.body.username,
+      //password: req.body.password
+      //password: md5(req.body.password)
+      password: hash
+    });
+    user.save(function(err){
+      if(!err){
+        console.log("Successfully registerd");
+        res.render("secrets");
+      }
+      else{
+        console.log(err);
+      }
+    });
   });
 });
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = md5(req.body.password); //req.body.password;
+  const password = req.body.password;
+  //const password = md5(req.body.password);
 
   User.findOne({email:username}, function(err, foundUser){
     if(!err){
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
-        else{
-          console.log("Incorrect username or password");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if(result === true){
+            res.render("secrets");
+          }else{
+            console.log("Incorrect username or password");
+          }
+        });
+
       }
       else{
         console.log("Incorrect username or password");
